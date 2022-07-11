5e96983fa9f93ce5226e484485dc1a9cd1d3c5dc
liseenko <liseenko.a@gmail.com>
2022-07-11 13:57:23 +0500

    final release

--- js/animation.js
+++ js/animation.js
@@ -51,3 +51,20 @@ function updateScroll(){
   }
 }
 window.addEventListener('scroll', updateScroll);
+
+function addSmoothScroll(link) {
+  link.addEventListener('click',onLinkClick)
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  addSmoothScroll(link);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
